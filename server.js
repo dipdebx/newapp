ffb6d5b0651b3f92dca618cfd35413252de1770c
dipdebx <deb.dipanjan@gmail.com>
2016-11-21 13:11:30 +0530

    server.js changed

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ var app = express();
 app.use(morgan('combined'));
 
 app.get('/', function (req, res) {
-	//res.sendFile(path.join(__dirname, 'ui', 'index.html'));
-	res.send("<html><body><h1>Hello World!</h1></body></html>");
+	res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+	//res.send("<html><body><h1>Hello World!</h1></body></html>");
 });
 
 app.set('port', process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080);
